81a321c3c6154327c3c5972004c221cd8ba4b90c
kranthi kiran <imkranthik@gmail.com>
2023-04-26 14:06:26 +0530

    Update test-alb.tf

--- test-alb.tf
+++ test-alb.tf
@@ -39,6 +39,6 @@ resource "aws_lb_listener" "example" {
 resource "aws_elb_attachment" "example" {
   load_balancer_arn = aws_lb.example.arn
   target_group_arn  = aws_lb_target_group.example.arn
-  instance       = ["i-0b73c6d866fe1dd5a" , "i-0b7120b20f0e8990f"] 
+  instance       = ["i-0b73c6d866fe1dd5a"] 
 }
 
