4139b87ee33369fc72fef7ff11a90f5e52dd75c4
kranthi kiran <imkranthik@gmail.com>
2023-04-26 15:46:39 +0530

    Update test-alb.tf

--- test-alb.tf
+++ test-alb.tf
@@ -15,7 +15,7 @@ resource "aws_lb_target_group" "example" {
   protocol          = "HTTP"
   vpc_id            = "vpc-08fd6d9e9b79fc0c9"
   target_type       = "instance"
-  target_id         = ["i-0b73c6d866fe1dd5a","i-0b7120b20f0e8990f"]
+  #target_id         = ["i-0b73c6d866fe1dd5a","i-0b7120b20f0e8990f"]
   health_check {
     protocol     = "HTTP"
     path         = "/"
@@ -40,7 +40,7 @@ resource "aws_lb_listener" "example" {
 resource "aws_elb" "example" {
   #load_balancer_arn = aws_lb.example.arn
   #target_group_arn  = aws_lb_target_group.example.arn
-  #instances       = ["i-0b73c6d866fe1dd5a","i-0b7120b20f0e8990f"]
+  instances       = ["i-0b73c6d866fe1dd5a","i-0b7120b20f0e8990f"]
   subnets            = ["subnet-0b7417c037d3b736f","subnet-08379ffadd1a6be7e"]
   listener {
     instance_port     = 80
@@ -51,8 +51,8 @@ resource "aws_elb" "example" {
 }
 
 resource "aws_elb_attachment" "example" {
-  load_balancer_arn = aws_lb.example.arn
-  target_group_arn  = aws_lb_target_group.example.arn
+  #load_balancer_arn = aws_lb.example.arn
+  #target_group_arn  = aws_lb_target_group.example.arn
   target_id         = ["i-0b73c6d866fe1dd5a","i-0b7120b20f0e8990f"]
 }
 
